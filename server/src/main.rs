ce8131cb4860d1766869f437ec4b39a2d90ffdf2
Shaun Karran <shaun.karran@gmail.com>
2016-12-26 23:52:24 +1000

    Add decoding of json formatted data from esp.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,27 @@
+extern crate chrono;
+extern crate rustc_serialize;
+
 use std::io::BufReader;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::str;
 use std::thread;
 
+use chrono::Local;
+use rustc_serialize::json;
 
-fn handle_client(client_id: u8, stream: TcpStream) {
+
+#[derive(Debug)] // Make printable.
+#[derive(RustcDecodable, RustcEncodable)] // Allows decoding of the sensor data.
+pub struct SensorData {
+    device_id: u8,
+    temperature: f32,
+}
+
+
+fn handle_client(stream: TcpStream) {
     let mut reader = BufReader::with_capacity(8, stream);
+    let mut sensor_data: SensorData;
 
     loop {
         let mut line = String::new();
@@ -20,20 +35,32 @@ fn handle_client(client_id: u8, stream: TcpStream) {
                 panic!("{:?}", e);
             }
         }
-        print!("Client {}: {}", client_id, line);
+
+        // Attempt to decode the recieved data.
+        // If it fails print the error and continue to waiting for the next message.
+        match json::decode(&line) {
+            Ok(decoded) => {
+                sensor_data = decoded;
+            }
+            Err(e) => {
+                println!("{:?}", e);
+                continue;
+            }
+        }
+
+        let local_time = Local::now();
+        println!("{}: {:?}", local_time.format("%d-%m-%Y %H:%M:%S").to_string(), sensor_data);
     }
 }
 
 fn main() {
-    let mut client_id: u8 = 0;
-    let listener = TcpListener::bind("127.0.0.1:12345").unwrap();
+    let listener = TcpListener::bind("192.168.1.12:12345").unwrap();
 
     // Accept connections and process them, spawning a new thread for each one.
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(move|| handle_client(client_id, stream));
-                client_id += 1;
+                thread::spawn(move|| handle_client(stream));
             }
             Err(e) => {
                 println!("{:?}", e); /* Connection failed. Print the error and continue. */
