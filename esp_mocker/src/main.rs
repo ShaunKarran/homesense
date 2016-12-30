eb73cd549c9da5c48e7403ebae03b6dd04e1435d
Shaun Karran <shaun.karran@gmail.com>
2016-12-30 12:34:02 +1000

    Update esp_mocker to send json formatted data.

--- esp_mocker/src/main.rs
+++ esp_mocker/src/main.rs
@@ -9,33 +9,26 @@ use std::time::Duration;
 use rand::distributions::{IndependentSample, Range};
 
 fn main() {
-    let mut writer;
-
-    // Attempt to connect to a host and `panic!`` if fail.
-    match TcpStream::connect("127.0.0.1:12345") {
-        Ok(stream) => {
-            writer = BufWriter::new(stream);
-        }
-        Err(e) => {
-            panic!("{:?}", e);
-        }
-    }
+    // Attempt to connect to a host and panic if failed.
+    let mut writer = BufWriter::new(TcpStream::connect("192.168.1.100:12345").unwrap());
 
     loop {
         let mut rng = rand::thread_rng();
         let number_between = Range::new(15, 30);
         let temperature = number_between.ind_sample(&mut rng).to_string();
 
-        let _ = writer.write_fmt(format_args!("{}\n", temperature));
+        let json = format!("{{\"device_id\": 100, \"temperature\": {}}}\n", temperature);
+
+        writer.write(json.as_bytes()).unwrap();
         match writer.flush() {
             Ok(_) => {
-                println!("Temperature: {}", temperature);
+                println!("Wrote: {}", json);
             }
             Err(e) => {
                 println!("{:?}", e); /* Write failed. Print the error and continue. */
             }
         }
 
-        sleep(Duration::from_secs(5));
+        sleep(Duration::from_secs(10));
     }
 }
