33057b365d4709c3032682fb0b8832822e6591eb
Shaun Karran <shaun.karran@gmail.com>
2017-01-09 22:55:35 +1000

    Fix webserver to actually display the latest reading.

--- homesense/src/bin/webserver.rs
+++ homesense/src/bin/webserver.rs
@@ -6,6 +6,7 @@ extern crate rocket;
 
 extern crate homesense;
 
+use diesel::expression::helper_types::Desc;
 use diesel::prelude::*;
 
 use homesense::models::Reading;
@@ -17,7 +18,9 @@ use homesense::utils;
 fn current_temp() -> String {
     let db_connection = utils::establish_db_connection();
 
+    // Use descending order and limit of 1 to get latest reading.
     let results = readings
+        .order(Desc::new(homesense::schema::readings::recorded_at))
         .limit(1)
         .load::<Reading>(&db_connection)
         .expect("Error loading readings");
