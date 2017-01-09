66612b5d307acfc7a089cf5adc58af519effbc3d
Shaun Karran <shaun.karran@gmail.com>
2017-01-09 10:44:40 +1000

    Add description of why recorded_at is able to be NULL.

--- server/src/models.rs
+++ server/src/models.rs
@@ -16,6 +16,11 @@ pub struct Reading {
 #[derive(Debug, Insertable, RustcDecodable)]
 #[table_name="readings"]
 pub struct NewReading {
+    // NOTE `recorded_at` is only an Option because I currently decode the JSON data from the esp
+    // directly into a `NewReading`. That JSON data does not contain `recorded_at` which is then
+    // added by the server. In short this means that `recorded_at` must be able to be None.
+    // Unfortunately that also means that the `recorded_at` field in the database by be able to be
+    // NULL, which is not ideal as every reading should contain a `recorded_at` value.
     pub recorded_at: Option<NaiveDateTime>,
     pub device_id: i32,
     pub temperature: Option<f32>,
